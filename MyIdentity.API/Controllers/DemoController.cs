e213d712e866505bd85de0dfae60a171ffe151f6
Gwen Demulder <31825690+scara1701@users.noreply.github.com>
2021-03-19 12:41:03 +0100

    Demo to test functionality of roles

---
+++ MyIdentity.API/Controllers/DemoController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyIdentity.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DemoController : ControllerBase
+    {
+        [Authorize(Roles ="Demo")]
+        [HttpGet]
+        public string GetById()
+        {
+            //zoek Id van ingelogde gebruiker
+            return "Welcome.";
+        }
+    }
+}
